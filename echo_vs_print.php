f58b94ff798ca2080a42c599ed3819e0b6cb3448
sohel5G <contactsohelrana@gmail.com>
2024-10-28 00:49:15 +0600

    echo vs print

---
+++ echo_vs_print.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>echo_vs_print</title>
+</head>
+<body>
+
+    <h4>
+
+    <?php 
+
+        echo "Hello <br/>";
+
+        $test = print("Hello");
+       
+        echo "<br/>";
+
+        echo $test;
+
+    ?>
+
+
+    </h4>
+
+</body>
+</html>
