5d15eea4c408eb1b370710e451690c2cd08fc42d
sohel5G <contactsohelrana@gmail.com>
2024-10-27 17:56:35 +0600

    new file added

---
+++ New.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Hello World</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam libero ex, optio consectetur tempora doloribus sint et quis quos ducimus vitae praesentium blanditiis nihil tempore, voluptas aut quas a dolorem, totam saepe! Illo voluptatem voluptatibus distinctio doloremque reiciendis, a quaerat. Autem optio eum blanditiis laudantium pariatur dolorum, perferendis doloremque consectetur.</p>
+
+    <?php
+
+        echo "Hello World";
+    ?>
+</body>
+</html>
+
