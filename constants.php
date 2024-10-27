5b9d00783a4ce56ca8f3dc97086989599695fa68
sohel5G <contactsohelrana@gmail.com>
2024-10-27 21:23:54 +0600

    Constants

---
+++ constants.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>constants.php</title>
+</head>
+<body>
+
+<h4>
+
+    <?php
+
+    define("NAME", "Sohel Rana");
+
+    echo NAME;
+
+
+    ?> 
+
+</h4>
+
+</body>
+</html>
