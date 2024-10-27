7ae5e8340bd522c26c1c134407043667be9350ba
sohel5G <contactsohelrana@gmail.com>
2024-10-27 17:26:20 +0600

    first commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Learning PHP</title>
+</head>
+<body>
+    
+    <?php  
+
+    $myCode = "Code";
+
+    echo "Hello World";
+
+    echo "<br/> OR <p>This is para</p> <br/>";
+
+    echo "So I have run this $myCode";
+
+    ?>
+
+
+    <div>
+        Footer area
+    </div>
+</body>
+</html>
