202b1af5efed99d1a5457367391c3b563b7fd43f
sohel5G <contactsohelrana@gmail.com>
2024-10-28 15:29:00 +0600

    printf

---
+++ printf.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Printf</title>
+</head>
+<body>
+    
+    <?php
+        $var_name = "Abdullah Al Alif";
+
+
+        echo $var_name;
+    ?>
+
+</body>
+</html>
