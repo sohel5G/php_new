41208d3bccfdb5d678aa46e17b18526e941b5c56
sohel5G <contactsohelrana@gmail.com>
2024-10-28 14:34:40 +0600

    Data type

---
+++ data_type.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Data type</title>
+</head>
+<body>
+    
+    <div>
+        <h1>Data type here</h1>
+        <?php
+            $var_1 = "Araf"; // String
+            $var_2 = 123;  // integer
+            $var_3 = 123.34; // Float / Decimal 
+            $var_4 = true; // false; // Boolian
+            $var_5 = null;
+
+        ?>
+
+    </div>
+
+</body>
+</html>
+
