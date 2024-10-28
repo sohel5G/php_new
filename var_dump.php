0b4a675cad973baf218d4a6c745aac98096a6bae
sohel5G <contactsohelrana@gmail.com>
2024-10-28 14:44:08 +0600

    var dump

---
+++ var_dump.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Var_dump</title>
+</head>
+<body>
+    
+    <h1>Var Dump</h1>
+
+    <?php
+        $name = "Abdullah Al Araf";
+
+        var_dump($name);
+
+    ?>
+
+</body>
+</html>
