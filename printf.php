2265f035900ac6aa4a316d7a3b786417f0d02cc0
sohel5G <contactsohelrana@gmail.com>
2024-10-28 16:44:53 +0600

    done

--- printf.php
+++ printf.php
@@ -17,7 +17,24 @@
         $capital_Later = ucwords($var_name); // first case capital
         echo $capital_Later . "<br/>";
 
-        printf("hello %s, What's your school name in %s?", $var_name, $school_name);
+        printf("hello %s, What's your school name in %s?", ucwords($var_name), $school_name);
+
+        echo "<br/>";
+        
+        $fname = "abdullah";
+        $lname = "alif";
+
+        $html_block = "
+            <div>
+                <h2> %s al %s</h2>
+            </div>
+        
+        ";
+
+
+
+        printf($html_block, ucwords($fname), $lname);
+
 
 
     ?>
