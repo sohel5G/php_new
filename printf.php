2617bbf559d460d7f3973eb43c68b98d1eb37391
sohel5G <contactsohelrana@gmail.com>
2024-10-28 16:23:24 +0600

    ucword

--- printf.php
+++ printf.php
@@ -8,10 +8,18 @@
 <body>
     
     <?php
-        $var_name = "Abdullah Al Alif";
+        $var_name = "abdullah al alif";
+        $school_name = "Kendua";
+
+        printf("hello $var_name");
+        echo "<br/>";
+
+        $capital_Later = ucwords($var_name); // first case capital
+        echo $capital_Later . "<br/>";
+
+        printf("hello %s, What's your school name in %s?", $var_name, $school_name);
 
 
-        echo $var_name;
     ?>
 
 </body>
