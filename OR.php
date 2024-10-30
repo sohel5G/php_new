978f2ec5a27142b83616268a7af11cc1ad08b1b8
sohel5G <contactsohelrana@gmail.com>
2024-10-30 21:08:13 +0600

    OR

---
+++ OR.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>OR Operator</title>
+</head>
+<body>
+    <?php
+          $user = "";
+          $age = 14;
+          $class = 12;
+  
+          $user = ( $age > 16 || $class > 10 ) ? "Admin" : "Guest";
+          echo $user;
+    ?>
+</body>
+</html>
