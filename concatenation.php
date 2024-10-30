9b1cc295946bf441349f04e36c9a12be6d843a10
sohel5G <contactsohelrana@gmail.com>
2024-10-30 17:59:18 +0600

    ASSIGNMENT

---
+++ concatenation.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Concatenation Operator</title>
+</head>
+<body>
+    <div>
+
+        <?php
+
+            $fname = "Abdullah Al";
+            $lname = "Alif";
+
+            $fname .= " $lname";
+
+            echo $fname;
+
+        ?>
+
+    </div>
+</body>
+</html>
