05315cdf59dd8a429d6b61d9bb9938213f296619
sohel5G <contactsohelrana@gmail.com>
2024-10-30 18:15:29 +0600

    Spacedhip operator

---
+++ spaceship.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Spaceship</title>
+</head>
+<body>
+    
+    <?php
+    
+    $var_num1 = 10;
+    $var_num2 = 5;
+
+    echo $var_num1 <=> $var_num2;
+    echo "<br/>";
+    var_dump($var_num1 <=> $var_num2);
+
+    ?>
+
+</body>
+</html>
