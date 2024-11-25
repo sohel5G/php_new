b0e4206abc4de12937f1bb7ceff5837a0a2c06b6
sohel5G <contactsohelrana@gmail.com>
2024-11-25 12:29:35 +0600

    loop in a selcet tag

--- loop.php
+++ loop.php
@@ -9,16 +9,32 @@
     
     <?php
 
+       // While Loop
+
        $x = 1; 
        while($x <= 10){
         echo $x++ . "<br/>";
        }
 
-
-
-
-       
     ?>
+
+    <form action="#">
+        <h1>Choose Years</h1>
+        <select name="#" id="#">
+
+            <?php 
+                $year = 2000;
+                while($year < 2024){
+                    $year++;
+
+                    ?> <option value="#">Years <?php echo $year ?></option> <?php
+
+                }
+            ?>
+
+        </select>
+    </form>
+
 
 </body>
 </html>
