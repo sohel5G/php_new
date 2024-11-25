18bd1d52b1feeb72692f2d6fdc7702f895e06624
sohel5G <contactsohelrana@gmail.com>
2024-11-25 12:04:45 +0600

    Loop

---
+++ loop.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Loop</title>
+</head>
+<body>
+    
+    <?php
+
+       $x = 1; 
+       while($x <= 10){
+        echo $x++ . "<br/>";
+       }
+
+
+
+
+       
+    ?>
+
+</body>
+</html>
