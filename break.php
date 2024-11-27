4d432c5cc9d355aa25eb5f8d819831276693e32e
sohel5G <contactsohelrana@gmail.com>
2024-11-27 11:39:34 +0600

    Break

---
+++ break.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Break</title>
+</head>
+<body>
+    <?php
+        for ($i=1; $i <= 10; $i++) { 
+            if($i === 5){
+                break;
+            }
+            ?> 
+                <li>user <?php echo $i ?> </li> 
+            <?php
+        }
+    ?>
+</body>
+</html>
